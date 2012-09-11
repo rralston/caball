980f1281f56281071ed208af1269dfb1765688b8
Ben Skinner <benskinner@Bens-MacBook-Pro.local>
2012-09-11 17:56:58 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caball::Application.config.secret_token = '40dbe9daa16934cf1cd3b67c9f477d89b6ca90f8188cb945bbcd382dee05b59762d1e4ba5b64980d3f381de7dc19630f21ce5287f5275251af3b944b094856da'
