8d61701946c37c89997d343bd26630d7757e3a5a
Rupert Ralston <rralston@gmail.com>
2012-10-11 16:13:51 +1300

    Config Files update

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Caball::Application.routes.draw do
 
-  get "admin/index"
-
   match 'users/show' => 'users#show'
   match 'projects/show' => 'projects#show'
   
-  get "admin/index"
-  get "admin/interface"
-  get "admin/buttons"
-  get "admin/calendar"
-  get "admin/charts"
-  get "admin/chat"
-  get "admin/gallery"
-  get "admin/grid"
-  get "admin/invoice"
-  get "admin/login"
-  get "admin/tables"
-  get "admin/widgets"
-  get "admin/form_wizard"
-  get "admin/form_common"
-  get "admin/form_validation"
-  match ':permalink', :controller => 'admin', :action => 'show', :as => 'my_page'
+  namespace :admin do
+    %w[index interface buttons calendar charts chat gallery grid invoice login tables widgets form_wizard form_common form_validation].each do |page|
+      get 'admin/' + page
+    end
+  end
   
 end
