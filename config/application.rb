cc6930d158e53ca6193b4faae2b21a2c7ea7a845
Ben Skinner <benskinner@certoris.com>
2012-10-11 14:26:17 +1300

    Import fonts into asset pipeline and move to vendors/assets/fonts

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Caball
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Precompile additional asset types
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
