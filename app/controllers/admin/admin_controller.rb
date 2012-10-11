92e88f63e554d2cc597c02754d35a1cce8a1973d
Rupert Ralston <rralston@gmail.com>
2012-10-11 16:15:36 +1300

    Update Admin Controller and Model

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,34 @@
+class Admin::AdminController < Admin::BaseController
+  
+  def index
+  end
+  def interface
+  end
+  def buttons
+  end
+  def calendar
+  end
+  def charts
+  end
+  def chat
+  end
+  def gallery
+  end
+  def grid
+  end
+  def invoice
+  end
+  def login
+  end
+  def tables
+  end
+  def widgets
+  end
+  def form_wizard
+  end
+  def form_common
+  end
+  def form_validation
+  end
+end
+
