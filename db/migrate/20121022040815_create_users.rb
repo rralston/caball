b42e04c69c201f237e316b548ae41d0d6c177971
Rupert Ralston <rralston@gmail.com>
2012-10-31 19:36:12 +1300

    New Database Migration for User Table

--- db/migrate/20121022040815_create_users.rb
+++ db/migrate/20121022040815_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
      create_table :users do |t|
-       t.string :first_name
-       t.string :last_name
+       t.string :name
        t.string :email
        t.string :location
        t.float :latitude
@@ -11,6 +10,10 @@ class CreateUsers < ActiveRecord::Migration
        t.boolean :superadmin
        t.boolean :admin
        t.boolean :editor
+       t.string :provider
+       t.string :uid
+       t.string :oauth_token
+       t.datetime :oauth_expires_at
        t.timestamps
      end
    end
