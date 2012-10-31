e858eb644d6c9c45c6ac4202c57637833687c92f
Rupert Ralston <rralston@gmail.com>
2012-10-31 19:35:50 +1300

    New User Model For including Email on Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,18 @@ class User < ActiveRecord::Base
   has_one :photo
   has_many :videos
   accepts_nested_attributes_for :characteristics, :photo, :videos, :allow_destroy => true
-  attr_accessible :first_name, :last_name, :email, :location, :about, :characteristics_attributes, :photo_attributes, :photo, :videos_attributes
-  validates_presence_of :first_name, :last_name, :email, :message => "is required"
+  attr_accessible :name, :email, :location, :about, :characteristics_attributes, :photo_attributes, :photo, :videos_attributes
+  validates_presence_of :name, :email, :message => "is required"
   
   def self.from_omniauth(auth)
-      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.info.name
-        user.oauth_token = auth.credentials.token
-        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-        user.save!
-      end
-  end
+  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+     user.provider = auth.provider
+     user.uid = auth.uid
+     user.name = auth.info.name
+     user.oauth_token = auth.credentials.token
+     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+     user.email = auth.info.email
+     user.save!
+   end
+ end
 end
