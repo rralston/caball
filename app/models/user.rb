8b41fa6b8cab1aa97cad3127c4fde7d47016584a
Rupert Ralston <rralston@gmail.com>
2012-11-03 20:46:10 +1300

    Updated User to have normal name model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :message => "is required"
   
   acts_as_messageable
-  def name
-    email
-  end
   
   def self.from_omniauth(auth)
   where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
