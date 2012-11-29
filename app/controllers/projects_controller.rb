7ef3220640d8047b2f1f7a6c29356fb400c011dd
Rupert Ralston <rralston@gmail.com>
2012-11-29 15:14:02 +1300

    Project Show Controller Added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,10 @@ class ProjectsController < ApplicationController
   end
   
   def show
-    
+    @project = Project.find(params[:id])
+    if @project.nil?
+        redirect_to :action => :index
+    end
   end
   
   def edit
