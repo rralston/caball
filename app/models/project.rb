92c8982fea82b788c8e43828b22a44f085a73d31
Rupert Ralston <rralston@gmail.com>
2012-12-10 18:08:04 +1300

    New Project Model - Roles

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :roles
-  attr_accessible :title, :description, :start, :end
+  attr_accessible :title, :description, :start, :end, :roles_attributes
+  accepts_nested_attributes_for :roles, :allow_destroy => true  
   validates_presence_of :title, :description, :message => "is required"
 end
