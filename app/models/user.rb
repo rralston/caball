b332a558be9651dcd9711f307680cbf07a0e04e2
Rupert Ralston <rralston@gmail.com>
2012-12-15 19:58:12 +1300

    Deactivated part of FB login for easier testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,11 @@ class User < ActiveRecord::Base
   where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
      user.provider = auth.provider
      user.uid = auth.uid
-     user.name = auth.info.name
+     # user.name = auth.info.name
      user.oauth_token = auth.credentials.token
      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-     user.email = auth.info.email
+     # user.email = auth.info.email
+     # user.location = auth.info.user_hometown
      user.save!
    end
  end
