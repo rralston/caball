da44e092975849ecc5dc41c10c4d58e2b9cebee9
Rupert Ralston <rralston@gmail.com>
2012-12-20 20:34:30 +1300

    Deployment Configs 6

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,16 @@
-set :stages, %w(production staging)
-set :default_stage, "production"
-
 require "bundler/capistrano"
-require 'capistrano/ext/multistage'
 
 server "192.155.85.156", :web, :app, :db, primary: true
 
-set(:domain) { "#{application}.net" }
-set :ruby_version, "1.9.3-p194"
-
-set :user, "deployer"
 set :application, "caball"
+set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+
 set :scm, "git"
 set :repository, "git@github.com:rralston/#{application}.git"
 set :branch, "dev"
-set :port, 30128
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
@@ -31,4 +24,28 @@ namespace :deploy do
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
