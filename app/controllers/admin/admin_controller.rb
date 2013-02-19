9057a0f529c53ccd7b2de5c855119e8358aefe9c
Rupert Ralston <rralston@gmail.com>
2013-02-18 20:12:24 -0800

    Admin Module Initial Commit

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,9 @@ class Admin::AdminController < Admin::BaseController
   
   def index
     @users = User.all.count
+    @users_weekly = User.where('created_at >= ?', 1.week.ago).count
     @projects = Project.all.count
+    @projects_weekly = Project.where('created_at >= ?', 1.week.ago).count
   end
   def interface
   end
