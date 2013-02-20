3f0047857d1e50c54b5da0d165b0e44537f6ef9f
Max B <max@filmzu.com>
2013-02-20 22:43:49 +0000

    Updating user profile layout with helpers and modals.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  
+  def shorten(args)
+    length = args[:length]
+    text = args[:text]
+    partial = args[:partial]
+    output = truncate(text, :length => length, :omission => '...').html_safe
+    more_button = render :partial => partial
+    output += more_button if @user.about.size > 800
+    output
+  end
+  
 end
