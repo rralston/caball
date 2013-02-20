559cd05a919ba8afe70b6166dc4dd3b070cafb12
Rupert Ralston <rralston@gmail.com>
2013-02-19 23:00:28 -0800

    Google Analytics Setup on Production Server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,23 @@ module ApplicationHelper
   def alert
     flash[:alert]
   end
+  
+  def error
+    flash[:error]
+  end
+  
+  def google_analytics_js
+ 
+    content_tag(:script, :type => 'text/javascript') do
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-38653918-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    end if Rails.env.production?
+  end
 end
