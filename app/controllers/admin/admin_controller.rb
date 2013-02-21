94137b364e27cc5be14732b191fc2d5965cd9e79
Rupert Ralston <rralston@gmail.com>
2013-02-20 18:34:51 -0800

    Updated Admin Controller - New Pages & Count

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -20,18 +20,36 @@ class Admin::AdminController < Admin::BaseController
     @conversations_weekly = Conversation.where('created_at >= ?', 1.week.ago).count
   end
   def users
+    @count = User.all.count
     # @users = User.order("name").page(params[:page]).per(10)
     @search = User.search(params[:q])
     @users = @search.result.order("name").page(params[:page]).per(10)
   end
+  def user_images
+    @count = Photo.where('imageable_type = ?', User).count
+    # @users = User.order("name").page(params[:page]).per(10)
+    @search = Photo.search(params[:q])
+    @photos = @search.result.order("id").page(params[:page]).per(10)
+  end
+  def interrogate
+    @user = User.find(params[:id])
+    @projects = @user.projects
+    @talents = @user.talents.offset(1)
+    respond_to do |format|
+      format.html # show.html.erb
+    end
+  end
   def projects
+    @count = Project.all.count
     # @projects = Project.order("title").page(params[:page]).per(10)
     @search = Project.search(params[:q])
     @projects = @search.result.order("title").page(params[:page]).per(10)
   end
   def messages
+    @count = Notification.all.count
     @search = Notification.search(params[:q])
     @notification = @search.result.order("subject").page(params[:page]).per(10)
+    # @notification = Notification.order("subject").page(params[:page]).per(10)
   end
 end
 
