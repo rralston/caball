7a155a718c99335aae7d49a6963b8804669d6386
Max B <max@filmzu.com>
2013-02-22 07:03:00 +0000

    Fixed shorten text helper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
     partial = args[:partial]
     output = truncate(text, :length => length, :omission => '...').html_safe
     more_button = render :partial => partial
-    output += more_button if @user.about.size > 800
+    output += more_button if text.length > length
     output
   end
   
