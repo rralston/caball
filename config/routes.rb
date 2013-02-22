72b94abd0b4996373b1f190eefc687a913aaec5d
Rupert Ralston <rralston@gmail.com>
2013-02-22 14:20:14 -0800

    New Routes for Admin Image Galleries

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Caball::Application.routes.draw do
   
   # Admin Area
   namespace :admin do
-    %w[index users user_images interrogate projects messages interface buttons calendar charts chat gallery grid invoice login tables widgets form_wizard form_common form_validation].each do |page|
+    %w[index users user_images interrogate projects project_images messages interface buttons calendar charts chat gallery grid invoice login tables widgets form_wizard form_common form_validation].each do |page|
       get 'admin/' + page
     end
   end
