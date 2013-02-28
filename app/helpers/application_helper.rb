01de417bdd9583e2a1767cd8e9eb273b2f5d6370
Max B <max@filmzu.com>
2013-02-28 20:25:06 +0000

    Moved more modal helper from users helper to application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,18 @@ module ApplicationHelper
   def comment
     # use this keyword in the views, to comment-out stuff...
   end
+  
+  def shorten(args)
+    if args[:counter].present?
+      @counter = args[:counter]
+    end
+    length = args[:length]
+    text = args[:text]
+    partial = args[:partial]
+    output = truncate(text, :length => length, :omission => '...').html_safe
+    more_button = render :partial => partial
+    output += more_button if text.length > length
+    output
+  end
+  
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,4 @@
 module UsersHelper
   
-  def shorten(args)
-    if args[:counter].present?
-      @counter = args[:counter]
-    end
-    length = args[:length]
-    text = args[:text]
-    partial = args[:partial]
-    output = truncate(text, :length => length, :omission => '...').html_safe
-    more_button = render :partial => partial
-    output += more_button if text.length > length
-    output
-  end
-  
+
 end
