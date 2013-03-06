a513004238673833474a988df94714f6c2f6401b
Rupert Ralston <rralston@gmail.com>
2013-03-06 00:32:09 -0800

    Rollback Assets Precompile for Heroku

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Caball
   class Application < Rails::Application
     
     #Heroku Code to Help Fix Migration to Server
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
