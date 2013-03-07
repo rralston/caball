9db7b0dda231bba8bddbe7bffce5f0a69b3a57d9
Max B <max@filmzu.com>
2013-03-07 23:35:38 +0000

    Ajaxy follow button.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,12 +3,15 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship = current_user.friendships.create!(:friend_id => params[:friend_id])
-    redirect_to :back, :flash => {info: "You are now following #{User.find(@friendship.friend_id).name}"}
+    #new_link = current_user.friendships.find_by_friend_id(@user.id)
+    #redirect_to :back, alert: "You are no longer following #{User.find(@friendship.friend_id).name}"
+    render :json => {:success => true, :created =>true, :friendship => @friendship, :notice => "You are now following #{User.find(@friendship.friend_id).name}"}
   end
 
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    redirect_to :back, alert: "You are no longer following #{User.find(@friendship.friend_id).name}"
+    #redirect_to :back, alert: "You are no longer following #{User.find(@friendship.friend_id).name}"
+    render :json => {:success => true, :destroyed => true, :friendship => @friendship, :notice => "You are no longer following #{User.find(@friendship.friend_id).name}"}
   end
 end
