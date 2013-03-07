d4c72589a137cbe2d6af19516e36edff65ec800f
Max B <max@filmzu.com>
2013-03-07 00:19:07 +0000

    Config from Rupe->Max

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 # Rupe
-  provider :facebook, '157500387725769', '7d0ad29d6689c852a388434484485925'
+  # provider :facebook, '157500387725769', '7d0ad29d6689c852a388434484485925'
 # Max
-  # provider :facebook, '547686081928278', '6d797e8938754d5d5dc169297d6f96ba'
+  provider :facebook, '547686081928278', '6d797e8938754d5d5dc169297d6f96ba'
 end
