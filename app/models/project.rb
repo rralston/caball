d875f5922d5ad06f14948fce11720f3ab654260b
Rupert Ralston <rralston@gmail.com>
2013-03-06 18:38:43 -0800

    Featured User & Project Controls from Admin

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
   has_many :roles, :dependent => :destroy
   has_many :photos, :as => :imageable, :dependent => :destroy
   has_many :videos, :as => :videoable, :dependent => :destroy
-  attr_accessible :title, :description, :start, :end, :roles_attributes, :photos_attributes, :videos_attributes
+  attr_accessible :title, :description, :start, :end, :featured, :roles_attributes, :photos_attributes, :videos_attributes
   accepts_nested_attributes_for :roles, :photos, :videos, :allow_destroy => true  
   validates_presence_of :title, :description, :message => "is required"
 end
