823274891e6089507ccf42fc2dd1c2c3478a5e19
Max B <max@filmzu.com>
2013-03-11 05:25:16 +0000

    Big migration change that includes profiles and stuff in the code.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,11 @@
 class Photo < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
-  attr_accessible :image, :description, :content_type, :file_size, :updated_at
-  mount_uploader :image, ImageUploader
+  attr_accessible :image, :description, :content_type, :file_size, :updated_at, :md5hash
+  before_validation :compute_hash
+  validates_uniqueness_of :md5hash, :on => :create
+  mount_uploader :image, ImageUploader  
+
+  def compute_hash
+    self.md5hash = Digest::MD5.hexdigest(self.image.read)
+  end
 end
