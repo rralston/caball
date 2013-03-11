4a13e5b05b055f7e4a25ba7ed77bced0700dee04
Max B <max@filmzu.com>
2013-03-11 05:38:46 +0000

    Removing code I was trying to use to prevent multiple uploaded photos but that didn't work.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
-  attr_accessible :image, :description, :content_type, :file_size, :updated_at, :md5hash
-  before_validation :compute_hash
-  validates_uniqueness_of :md5hash, :on => :create
+  attr_accessible :image, :description, :content_type, :file_size, :updated_at
   mount_uploader :image, ImageUploader  
-
-  def compute_hash
-    self.md5hash = Digest::MD5.hexdigest(self.image.read)
-  end
 end
