99f0a971f4fad673cddb3f198ea8f4ec39b61c4f
ubuntu <ubuntu@ubuntu.(none)>
2013-03-14 13:13:29 -0700

    Fixing previous commit in profiles model.

--- app/models/profiles.rb
+++ app/models/profiles.rb
@@ -1,5 +1,5 @@
 class Profiles < ActiveRecord::Base
-  belongs_to :imageable, :user
+  belongs_to :imageable, :polymorphic => true
   attr_accessible :image, :file_name, :content_type, :file_size, :updated_at
   mount_uploader :image, ImageUploader
 end
