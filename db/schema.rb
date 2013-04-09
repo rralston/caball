f0998fa74883aca973b1b280b8649ef8d457c0e4
Rupert Ralston <rralston@gmail.com>
2013-04-09 20:44:09 +1200

    DB Migration User Blog Updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329014551) do
+ActiveRecord::Schema.define(:version => 20130409084115) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(:version => 20130329014551) do
   add_index "activities", ["owner_id", "owner_type"], :name => "index_activities_on_owner_id_and_owner_type"
   add_index "activities", ["recipient_id", "recipient_type"], :name => "index_activities_on_recipient_id_and_recipient_type"
   add_index "activities", ["trackable_id", "trackable_type"], :name => "index_activities_on_trackable_id_and_trackable_type"
+
+  create_table "blogs", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "blogs", ["user_id"], :name => "index_blogs_on_user_id"
 
   create_table "characteristics", :force => true do |t|
     t.integer  "age"
