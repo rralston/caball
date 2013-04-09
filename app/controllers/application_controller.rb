50f097f031665bc35d27d2f7206ac61b559f1d34
Rupert Ralston <rralston@gmail.com>
2013-04-09 13:31:08 +1200

    Application Controller Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #News Feed
+  include PublicActivity::StoreController
   before_filter :subdomain_view_path
   helper_method :current_user
   helper_method :notification
