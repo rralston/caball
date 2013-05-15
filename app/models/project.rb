8ee824ec9079d5ee7ffa9af8eba759458fa1194e
Max B <max@filmzu.com>
2013-05-14 17:58:23 -0700

    Set up roles to display properly for projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,29 @@ class Project < ActiveRecord::Base
   attr_accessible :title, :description, :start, :end, :featured, :roles_attributes, :photos_attributes, :videos_attributes, :status, :genre, :is_type, :thoughts, :compensation, :location
   accepts_nested_attributes_for :roles, :photos, :videos, :allow_destroy => true  
   validates_presence_of :title, :description, :message => "is required"
+  
+  def roles_percent
+    total_roles = roles.length
+    open_roles = 0
+    roles.each do |role|
+      if role.filled
+        open_roles += 1
+      end
+    end
+    if total_roles == 0
+      return 10
+    else
+      return ((open_roles.to_f / total_roles.to_f) * 10).to_i
+    end
+  end
+  
+  def open_roles
+    open_roles = 0
+    roles.each do |role|
+      if role.filled
+        open_roles += 1
+      end
+    end
+    return open_roles
+  end
 end
