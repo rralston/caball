bb9fb6bf7ff228a3b2f63ab9240467170bf2e9cb
Max B <max@filmzu.com>
2013-05-22 11:47:44 -0700

    Fixing some ugly controller code for activities.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
     search
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friend_ids, owner_type: "User").first(5)
   end
 end
