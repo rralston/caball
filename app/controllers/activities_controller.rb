f434c6143b3e6af07474b0657eade2933f4c196f
Anand Narayan <i@codeanand.com>
2013-07-03 17:40:11 +0530

    refactoed the activities model

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,8 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.order("created_at desc")
     search
+
+   # activity = User.find(activity.trackable.user).profiles.present?
   end
 end
---
+++ app/models/activity.rb
@@ -0,0 +1,8 @@
+class Activity < PublicActivity::Activity
+
+def user
+  User.find(trackable.user)
+end
+
+
+end
