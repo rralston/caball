b11829eceaacdfea5c33c37cecf9741e52e35c3c
Yuvaraja <yuv.slm@gmail.com>
2013-07-09 16:42:40 +0530

    love via remote

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,15 @@
 class LikesController < ApplicationController
 	def create
-		session[:back] = request.referer
 		current_user.likes.create(loveable_id: params[:like][:loveable_id], loveable_type: params[:like][:loveable_type])
-		redirect_to session[:back]
+		respond_to do |format|
+			format.js
+		end
 	end
 
 	def destroy
-		session[:back] = request.referer
 		Like.find(params[:id]).destroy
-		redirect_to session[:back]
+		respond_to do |format|
+			format.js
+		end
 	end
 end
