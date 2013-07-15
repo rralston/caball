50b0cd167ed9372a43c06598709ce04dec0477c1
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-07-15 19:04:25 +0530

    important dates design added to projects

--- spec/models/project_date_spec.rb
+++ spec/models/project_date_spec.rb
@@ -9,5 +9,21 @@ describe ProjectDate do
   context "Associations" do
     it { should belong_to(:project) }
   end
+
+  context "date formatting methods" do
+    before(:all){
+      @p_date = FactoryGirl.create(:project_date, :date_time => '2013-12-18 14:55')
+    }
+    subject { @p_date }
+
+    its(:full_date) { should == '2013-12-18' }
+    its(:date) { should == '18' }
+    its(:year) { should == '2013' }
+    its(:month) { should == '12' }
+    its(:month_year) { should == 'Dec \'13' }
+
+    its(:time) { should == '14:55' }
+    its(:formatted_time) { should == '02:55pm' }
+  end
   
 end
