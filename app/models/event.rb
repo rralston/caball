467d93e315d4510422640fe35e2f55955363c9c4
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-07-18 19:19:20 +0530

    conditional assosications issue fixed for postgres

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,24 +8,24 @@ class Event < ActiveRecord::Base
 
   has_many :likes, :as => :loveable, :dependent => :destroy
   has_many :fans, :through => :likes, :source => :user
-  has_one :main_photo, :class_name => 'Photo', :as => :imageable, :dependent => :destroy, :conditions => 'is_main = 1'
+  has_one :main_photo, :class_name => 'Photo', :as => :imageable, :dependent => :destroy, :conditions => { :is_main => true }
   
   has_many :other_photos, :class_name => 'Photo', :as => :imageable,
-           :dependent => :destroy, :conditions => 'is_main = 0'
+           :dependent => :destroy, :conditions => { :is_main => false }
   
   has_many :videos, :as => :videoable, :dependent => :destroy
   has_many :comments, :as => :commentable, :dependent => :destroy
   
   has_many :other_important_dates, :class_name => 'ImportantDate',
-           :as => :important_dateable, :dependent => :destroy, :conditions => 'is_start_date = 0 && is_end_date = 0'
+           :as => :important_dateable, :dependent => :destroy, :conditions => { :is_start_date => false, :is_end_date => false }
 
   has_many :likers, :through => :likes, :source => :user
 
   has_one :start, :class_name => 'ImportantDate', :as => :important_dateable,
-          :dependent => :destroy, :conditions => 'is_start_date = 1'
+          :dependent => :destroy, :conditions => { :is_start_date => true }
   
   has_one :end, :class_name => 'ImportantDate', :as => :important_dateable,
-          :dependent => :destroy, :conditions => 'is_end_date = 1'
+          :dependent => :destroy, :conditions => { :is_end_date => true }
 
   has_many :attends, :as => :attendable, :dependent => :destroy
   # has_many :attendees, :through => :attends, :source => :user
