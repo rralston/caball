c6bd128b86ee5a62c6c70388bac042351f28edc7
Rupert Ralston <rralston@gmail.com>
2013-08-13 14:53:28 -0700

    Restore of production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,23 +5,21 @@ Caball::Application.configure do
   #   'secret' == password
   # end
   
-  config.assets.initialize_on_precompile = true
-  
   # Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -83,5 +81,5 @@ Caball::Application.configure do
    }
   
   # Change when Push to the Website or will Error out
-  config.action_mailer.default_url_options = { :host => 'obscure-temple-3104.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mysterious-brook-4528.herokuapp.com' }
 end
