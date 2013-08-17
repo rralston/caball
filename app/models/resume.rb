b3f69fa809c5b885ef7e92dea75af3334bfb3b8e
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-08-17 17:38:51 +0530

    deleted the resume model

--- app/models/resume.rb
+++
@@ -1,5 +0,0 @@
-class Resume < ActiveRecord::Base
-  belongs_to :documentable, :polymorphic => true
-  attr_accessible :document, :description, :content_type, :file_size, :updated_at
-  mount_uploader :document, DocumentUploader
-end
