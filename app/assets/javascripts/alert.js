1bd0a44fd08fb2e96e8aad0620a9a2c26f312a49
Yuvaraja <yuv.slm@gmail.com>
2013-08-30 16:12:36 +0530

    modified the flash message

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -3,6 +3,6 @@ var Alert = {
     $('#bootstrap-alert-placeholder').html('<div class="alert alert-' + type + '"><a class="close" data-dismiss="alert">×</a><span>'+notice+'</span></div>');
     $('.alert').alert();
     // We have to set our own timeout for closing the alert
-    window.setTimeout(function() { $(".alert").alert('close'); }, 2000);
+    window.setTimeout(function() { $(".alert").alert('close'); }, 10000);
   }
 }
