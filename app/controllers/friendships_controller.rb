e74cd1551e934e768285f55a555016b6ae4e9437
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-09-03 16:05:13 +0530

    fans duplications destroy fix

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -18,12 +18,11 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = current_user.friendships.where(:friend_id => params[:friend_id]).first
-    @friendship.destroy
+    @friendships = current_user.friendships.where(:friend_id => params[:friend_id]).destroy_all  # if at all there are dupplicate friendships, they will be destroyed.
     render :json => { :destroyed => true,
                       :follower => @current_user.to_json(),
-                      :followers_count => @friendship.friend.followers.count,
-                      :notice => "You are no longer following #{User.find(@friendship.friend_id).name}"
+                      :followers_count => @friendships.first.friend.followers.count,
+                      :notice => "You are no longer following #{User.find(@friendships.first.friend_id).name}"
                     }
   end
 end
