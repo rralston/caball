c222dc9500f65832ed0ae7dd347ad42a07e105f0
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2013-09-18 19:18:18 +0530

    compilateion issue tried to be fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,13 @@ Caball::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin/admin.css admin/*.js )
-  config.assets.precompile += %w( *.css *.js )
+
+  config.assets.precompile += %w( conversations/new.css users/users_manifest.js dashboard/dashboard.css dashboard/dashboard_manifest.js users/users.js signup.css signin.css
+                                events_manifest.js events/manifest.css events/index.css events/events_manifest.js home_page.css projects/index.css
+                                users/index.css application.js 
+                                projects/projects_manifest.js projects/manifest.css users/user_search.js projects/index.css 
+                                static_pages/our_story.css 
+                                users/users_manifest.js users/manifest.css users/index.css users/show.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
