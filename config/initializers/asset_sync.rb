ae15be8ed639856b09f4f5743c7a65e33a30448c
Rupert Ralston <rralston@gmail.com>
2013-09-24 11:35:13 -0700

    Fog Details for Asset Sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,8 @@
 # AssetSync.configure do |config|
-#   config.fog_provider = 'AWS'
-#   config.fog_directory = 'filmzu'
-#   config.aws_access_key_id = 'AKIAJHIBHMAKIHAHLSJQ'
-#   config.aws_secret_access_key = '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'
+  config.fog_provider = 'AWS'
+  config.fog_directory = 'filmzu'
+  config.aws_access_key_id = 'AKIAJHIBHMAKIHAHLSJQ'
+  config.aws_secret_access_key = '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'
 
 #   # Don't delete files from the store
 #   # config.existing_remote_files = 'keep'
