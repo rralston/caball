00254c93d90502f9a61c4eb76dfd54b890d47346
Rupert Ralston <rralston@gmail.com>
2013-09-23 20:38:54 -0700

    Asset Compile Update

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,35 +1,3 @@
-// Custom scripts goes here
-(function() {
-    
-    // Initialize carousel
-    carouselInit();  
-
-    // Portfolio filters function
-    portfolioFilters();
-
-})();
-
-
-// Function to animate the height of carousel in case of slides with different heights
-function carouselInit() {
-    var carousel = $('#myCarousel'),
-        defaultHeight = carousel.find('.active').height();
-
-    // setting the default height
-    carousel.css('min-height', defaultHeight);
-
-    // animate the container height on any slider transitiom
-    carousel.bind('slid', function() {
-        var itemheight = carousel.find('.active').height();
-
-        carousel.css('min-height', itemheight);
-        carousel.animate({
-            height: itemheight
-        }, 50 );
-    });
-}
-
-
 // Function to style the map in the contact page, change lat and lng vars to create your own map
 function mapInit() {
     // Create an array of styles.
@@ -86,27 +54,4 @@ function mapInit() {
         //Associate the styled map with the MapTypeId and set it to display.
         map.mapTypes.set('map_style', styledMap);
         map.setMapTypeId('map_style');
-}
-
-function portfolioFilters() {
-    var filters = $('.thumbnail-filters');
-    
-    filters.on('click', 'a', function(e) {
-        var active = $(this),
-            portfolio = filters.next();
-            activeClass = active.data('filter');
-
-        
-        filters.find('a').removeClass('active');
-        active.addClass('active');
-        
-        if ( activeClass == 'all') {
-            portfolio.find('li').removeClass('inactive');
-        } else {
-            portfolio.find('li').removeClass('inactive').not('.filter-' + activeClass ).addClass('inactive');
-        }
-        
-
-        e.preventDefault();
-    });
 }
