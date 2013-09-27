bb587712546871b55084214be44bcc1c87984aff
Rupert Ralston <rralston@gmail.com>
2013-09-26 20:33:43 -0700

    New Relic Allowed + 4 Workers

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,12 +9,11 @@
 # worker_processes 2
 # timeout 30
 
-worker_processes 3
+worker_processes 4
 timeout 30
-preload_app false
+preload_app true
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -22,12 +21,11 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-end
+end 
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
