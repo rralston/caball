c28f6f1bb3e3eb03d7d641e17f8ac6e14681ca67
Rupert Ralston <rralston@gmail.com>
2013-10-22 19:35:19 +1300

    Sessions Logic to Catch those with Incomplete Profiles

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,11 @@ class Users::SessionsController < Devise::SessionsController
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    redirect_back_or(after_sign_in_path_for(@user))
+    if @user.profile == nil || @user.location == nil || @user.location == ""
+      redirect_to edit_user_path(@user)
+    else
+      redirect_back_or(after_sign_in_path_for(@user))
+    end
   end
 
 end
