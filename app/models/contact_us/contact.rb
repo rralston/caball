0bf7a20e8adaafb84dbc4e89b88135ab1f294664
Rupert Ralston <rralston@gmail.com>
2013-12-04 11:52:29 +1300

    Verification on Contact Us that Real Name is used

---
+++ app/models/contact_us/contact.rb
@@ -0,0 +1,32 @@
+class ContactUs::Contact
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :email, :message, :name, :subject
+
+  validates :email,   :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+                      :presence => true
+  validates :message, :presence => true
+  validates :name,    :format => { :with => /^([a-z]+ ){1,2}[a-z]+$/i },
+                      :presence => {:if => Proc.new{ContactUs.require_name}}
+  validates :subject, :presence => {:if => Proc.new{ContactUs.require_subject}}
+
+  def initialize(attributes = {})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def save
+    if self.valid?
+      ContactUs::ContactMailer.contact_email(self).deliver
+      return true
+    end
+    return false
+  end
+
+  def persisted?
+    false
+  end
+
+end
