2715adf548838a6d8446dc7aed0dd91cf3011f1d
Rupert Ralston <rralston@gmail.com>
2014-03-02 12:45:55 -0800

    Step 1 Link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,7 @@ Caball::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-  config.assets.precompile += %w[ *.png *.jpeg *.jpg *.gif ]
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
