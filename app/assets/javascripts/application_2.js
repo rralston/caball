340967740bc18b3986d37c6760791bf547097872
Rupert Ralston <rralston@gmail.com>
2014-03-03 16:07:25 -0800

    Removed Handlebars and Ember from Precompile

--- app/assets/javascripts/application_2.js
+++ app/assets/javascripts/application_2.js
@@ -4,10 +4,5 @@
 //= require bootstrap-slider
 //= require pages
 
-//= require handlebars
-//= require ember
-//= require ember-data
 //= require_self
 //= require caball
-
-window.Filmzu = Ember.Application.create()
