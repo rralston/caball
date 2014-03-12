3321aa436eedf3db01c50c1e5fc0c7ab4e540834
Haris <haris@miavision.net>
2014-03-12 13:33:26 +0100

    Controller name

--- app/controllers/api/v1/film_makers_controller.rb
+++ app/controllers/api/v1/film_makers_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ProjectsController < BaseController
+    class FilmMakersController < BaseController
       def list_film_makers
         @users=User.all
         render :json=>@users , :each_serializer=>Api::V1::Serializers::FilmMakerSerializer
