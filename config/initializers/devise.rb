7f9bfbf099b38e5fb99da536360df3f0e076f9e8
Rupert Ralston <rralston@gmail.com>
2014-03-12 15:32:54 -0700

    Environmental Keys working for Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = APP["mailer_sender"]
-  config.secret_key = APP["devise_secret_key"]
+  config.secret_key = ENV['devise_secret_key']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
